a0de09745c1cb597bf97b58f38d9ecf2b061a3d0
ggsimao <cd_giulio@hotmail.com>
2021-08-13 19:54:51 -0300

    initial skeleton of first class (Grammar)

---
+++ regular_languages/target/debug/deps/main-da9bf590532a3229.d
@@ -0,0 +1,5 @@
+/home/acerola/Desktop/Formal-Languages-and-Compilers-INE5421-Assignment-1-Rust-Rework/regular_languages/target/debug/deps/main-da9bf590532a3229.rmeta: src/bin/main.rs
+
+/home/acerola/Desktop/Formal-Languages-and-Compilers-INE5421-Assignment-1-Rust-Rework/regular_languages/target/debug/deps/main-da9bf590532a3229.d: src/bin/main.rs
+
+src/bin/main.rs:
---
+++ regular_languages/target/debug/deps/regular_languages-307555ee74860daf.d
@@ -0,0 +1,6 @@
+/home/acerola/Desktop/Formal-Languages-and-Compilers-INE5421-Assignment-1-Rust-Rework/regular_languages/target/debug/deps/regular_languages-307555ee74860daf.rmeta: src/lib.rs src/regular_grammars.rs
+
+/home/acerola/Desktop/Formal-Languages-and-Compilers-INE5421-Assignment-1-Rust-Rework/regular_languages/target/debug/deps/regular_languages-307555ee74860daf.d: src/lib.rs src/regular_grammars.rs
+
+src/lib.rs:
+src/regular_grammars.rs:
