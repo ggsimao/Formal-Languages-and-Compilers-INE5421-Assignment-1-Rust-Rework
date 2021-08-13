b3e4efcb8ece06753f2e2a1531e1cc261942665c
ggsimao <cd_giulio@hotmail.com>
2021-08-13 19:54:51 -0300

    initial skeleton of first class (Grammar)

---
+++ regular_languages/src/bin/main.rs
@@ -0,0 +1,5 @@
+use regular_languages::regular_grammars::Grammar;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ regular_languages/src/regular_grammars.rs
@@ -0,0 +1,53 @@
+use std::fmt::Display;
+
+pub struct Grammar {
+    initialSymbol: char,
+    productions: Vec<Production>,
+    name: String,
+}
+
+pub struct Production {
+    leftSide: char,
+    rightSide: (char, char),
+}
+
+// impl Display for Grammar {
+
+// }
+
+impl Grammar {
+    pub fn new(&self, initialSymbol: char, mut productions: Vec<Production>, name: String) -> Grammar {
+        productions = if productions.len() == 0 {
+            vec!(Production::new('S', "aS"), Production::new('S', "a"))
+        } else {
+            Grammar::validate_productions(productions)
+        };
+
+        Grammar{initialSymbol, productions, name}
+    }
+
+    pub fn validate_productions(productions: Vec<Production>) -> Vec<Production> {
+        // TODO
+        productions
+    }
+
+    pub fn produce(&self, size: i32) -> Vec<String> {
+        // TODO
+        vec!("kkk".to_string())
+    }
+}
+
+impl Production {
+    pub fn new(leftSide: char, rightSide: &str) -> Production {
+        let prodRight = match rightSide.len() {
+            0 => ('&', '&'),
+            1 => (rightSide.chars().nth(0).unwrap(), '&'),
+            2 => {
+                let mut it = rightSide.chars();
+                (it.next().take().unwrap(), it.next().take().unwrap())
+            },
+            _ => panic!("Production is too long!")
+        };
+        Production{leftSide, rightSide: prodRight}
+    }
+}
